c75c3a1409cddc0d5c01f2cd546d804d4ef8ec5a
Daniel Andrade <andrade.daniel111@gmail.com>
2019-07-01 09:42:45 -0700

    finsihing touches

---
+++ public/precache-manifest.b60271a7690366c2e36ebc08631ef996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39066d608bfbb52e9d9419dd4b3e0a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b9f122949c7c04d3073",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "291d01be969e2902ae01",
+    "url": "/static/js/2.dba5ff92.chunk.js"
+  },
+  {
+    "revision": "0b9f122949c7c04d3073",
+    "url": "/static/js/main.9e39047f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
